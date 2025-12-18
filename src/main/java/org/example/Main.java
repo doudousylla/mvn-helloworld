df53c38fae5727f298828eafa999a3539492d972
Mahamadou Sylla <mahamadousylla167@gmail.com>
2025-12-18 23:37:10 +0100

    commit it

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,18 @@
 package org.example;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class Main {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+   // private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        LOGGER.info("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            LOGGER.debug("i = {}", i);
+            System.out.println("i = " + i);
         }
     }
 }
